23fb6873bfa4a96a5b5ba9d6cecc22527d13afa2
Ayesha Zaheer <zayesha@amazon.com>
2022-01-07 14:36:02 -0800

    S3 bucket encruption

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "cognito81d9f49f": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "AppClientSecret": "string"
+        }
+    }
+}
